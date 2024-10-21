38b9aa143e58df3baac9ceffa695631a5d7b715c
Dhruv Naik <dhruviscracked@gmail.com>
2024-10-21 18:55:44 +0530

    Cleaned up appwrite config

--- src/appwrite/appwrite.ts
+++ src/appwrite/appwrite.ts
@@ -1,24 +1,18 @@
 import { Client, Account, OAuthProvider, Databases, ID, Query, Storage } from "appwrite";
-import { useUser } from "@clerk/nextjs";
+import { appwriteConfig } from "./config";
 
 export const client = new Client();
 
 client
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject('670f8c83000b3c68feee') 
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId) 
  
- const account = new Account(client);
  const databases = new Databases(client);
- const databaseId = '6710996a000569b8a6ad';
- const userCollectionId = '671099c3003764136a39';
- const videoCollectionId = '6715235b003bff9c5f45';
- const bucketId = '6715256f0020032941dd';
-
  const storage = new Storage(client);
 
  export async function uploadFile(file: File) {
   try {
-    const response = await storage.createFile(bucketId, ID.unique(), file);
+    const response = await storage.createFile(appwriteConfig.bucketId, ID.unique(), file);
     return response;
   } 
   catch (error) {
@@ -34,13 +28,13 @@ client
   try {
     let videoResponse, fileUrl;
 
-    videoResponse = await storage.createFile(bucketId, ID.unique(), videoFile);
+    videoResponse = await storage.createFile(appwriteConfig.bucketId, ID.unique(), videoFile);
 
     fileUrl = await getFilePreview(videoResponse.$id);
 
     const videoDoc = await databases.createDocument(
-      databaseId,
-      videoCollectionId,
+      appwriteConfig.databaseId,
+      appwriteConfig.videoCollectionId,
       ID.unique(),
       {
         title,
@@ -50,16 +44,16 @@ client
     );
 
     console.log('Video data successfully stored in Appwrite:', videoDoc);
-    return videoDoc; // Return the created document
+    return videoDoc; 
   } catch (error) {
     console.error('Error storing video data:', error);
-    throw error; // Rethrow the error if needed
+    throw error; 
   }
 }
  
  export async function getFilePreview(fileId: string) {
   try {
-    let fileUrl = await storage.getFileView(bucketId, fileId);
+    let fileUrl = await storage.getFileView(appwriteConfig.bucketId, fileId);
 
     if(!fileUrl){ throw new Error('File not found')}
 
@@ -71,19 +65,18 @@ client
 
  export const storeUserInDatabase = async (userId: string, name: string, email: string) => {
   try {
-    // Check if the user already exists using Query
+   
     const userExists = await databases.listDocuments(
-      '6710996a000569b8a6ad',  // Your Database ID
-      '671099c3003764136a39', // Your Collection ID
-      [Query.equal('userId', userId)] // Use Query.equal for filtering
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal('userId', userId)] 
     );
 
-    // If the user doesn't exist, create a new document in Appwrite
     if (userExists.total === 0) {
       const userDoc = await databases.createDocument(
-        '6710996a000569b8a6ad',  // Your Database ID
-        '671099c3003764136a39', // Your Collection ID
-        'unique()',  // Auto-generate document ID
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        'unique()',  
         {
           userId,
           name,
@@ -99,49 +92,6 @@ client
     console.error('Error storing user data:', error);
   }
 };
-
-  // export const loginWithGoogle = async () => {
-  //   try {
-  //       await account.createOAuth2Session(
-  //       OAuthProvider.Google,
-  //       'http://localhost:3000/home',
-  //       'http://localhost:3000/fail',
-  //       ['email', 'profile', 'openid']
-  //     )
-    
-  //   } catch (error) {
-  //     console.error('OAuth login error:', error);
-  //   }
-  // }
-  
-  // export const logoutUser = async () => {
-  //   try {
-  //     await account.deleteSession('current')
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
-  
-  // export const getUser = async () => {
-  //   try {
-  //     return await account.get()
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
-
-  // export const getCurrentSession = async () => {
-  //   try {
-  //     const session = await account.getSession('current');
-  //     console.log('Provider:', session.provider);
-  //     console.log('Provider UID:', session.providerUid);
-  //     console.log('Provider Access Token:', session.providerAccessToken);
-  //     return session;
-  //   } catch (error) {
-  //     console.error("Error fetching session:", error);
-  //     return null;
-  //   }
-  // };
 
 export { ID } from 'appwrite';
 export { OAuthProvider } 
---
+++ src/appwrite/config.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+    endpoint: 'https://cloud.appwrite.io/v1',
+    projectId: '670f8c83000b3c68feee',
+    databaseId: '6710996a000569b8a6ad',
+    userCollectionId: '671099c3003764136a39',
+    videoCollectionId: '671099c3003764136a39',
+    bucketId: '6715256f0020032941dd'
+}
