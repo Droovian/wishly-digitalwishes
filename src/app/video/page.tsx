03fa6062ebfb36c41a66d2d19397b72f79bb1b7f
gadekar123 <gadekarshivam111@gmail.com>
2024-10-21 22:59:03 +0530

    Refactor Video component to pass spaceId prop to VideoUploadForm

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import VideoUploadForm from '@/components/VideoForm'
 
-const Video = () => {
+const Video = (spaceId:string) => {
   return (
     <div className='p-5 flex justify-center items-center'>
-        <VideoUploadForm />
+        <VideoUploadForm spaceId={spaceId} />
     </div>
   )
 }
