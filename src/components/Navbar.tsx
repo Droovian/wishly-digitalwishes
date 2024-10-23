b790eece179fbcfc1e717e7c0bbf07c0f1e921cd
Dhruv Naik <dhruviscracked@gmail.com>
2024-10-23 16:21:11 +0530

    PATCH/build errors

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
             <div className='invisible sm:visible border rounded-xl p-3'>
                 <ul className='flex font-bold text-gray-600 text-sm space-x-4 mx-10'>
                     <Link href={"/create"} className='hover:text-gray-800'>Start a space</Link>
-                    <Link href={"/contact"} className='hover:text-gray-800'>about</Link>
+                    <Link href={"/about"} className='hover:text-gray-800'>about</Link>
                 </ul>
             </div>
             <div className="flex items-center">
