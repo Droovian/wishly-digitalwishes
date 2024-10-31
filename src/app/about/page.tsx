b4a1c5f0a9f9c32ad560d1172102e2099f847b5e
gadekar123 <gadekarshivam111@gmail.com>
2024-10-31 19:33:47 +0530

    Add details prop to About component and rename video URL key

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,25 +3,30 @@ import VideoGroupMessage from "@/components/templates/video-group-carousel"
 const videos = [
     {
         id: '1',
-        url: 'https://cloud.appwrite.io/v1/storage/buckets/6715256f0020032941dd/files/67189bee0023019a2737/view?project=670f8c83000b3c68feee&project=670f8c83000b3c68feee&mode=admin',  // Path to your video in the public folder
+        video: 'https://cloud.appwrite.io/v1/storage/buckets/6715256f0020032941dd/files/67189bee0023019a2737/view?project=670f8c83000b3c68feee&project=670f8c83000b3c68feee&mode=admin',  // Path to your video in the public folder
         title: 'Sample Video'
     },
     {
         id: '2',
-        url: 'https://cloud.appwrite.io/v1/storage/buckets/6715256f0020032941dd/files/67189bee0023019a2737/view?project=670f8c83000b3c68feee&project=670f8c83000b3c68feee&mode=admin',  // Path to your video in the public folder
+        video: 'https://cloud.appwrite.io/v1/storage/buckets/6715256f0020032941dd/files/67189bee0023019a2737/view?project=670f8c83000b3c68feee&project=670f8c83000b3c68feee&mode=admin',  // Path to your video in the public folder
         title: 'Sample'
     },
     {
         id: '3',
-        url: 'https://cloud.appwrite.io/v1/storage/buckets/6715256f0020032941dd/files/67189bee0023019a2737/view?project=670f8c83000b3c68feee&project=670f8c83000b3c68feee&mode=admin',  // Path to your video in the public folder
+        video: 'https://cloud.appwrite.io/v1/storage/buckets/6715256f0020032941dd/files/67189bee0023019a2737/view?project=670f8c83000b3c68feee&project=670f8c83000b3c68feee&mode=admin',  // Path to your video in the public folder
         title: 'Sample'
     },
 ]
 
+const details = {
+    name:"Shivam",
+
+}
+
 export default function About(){
     return (
         <div className="flex justify-center h-screen">
-            <VideoGroupMessage videos={videos} />
+            <VideoGroupMessage videos={videos} details={details} />
         </div>
     )
 }
